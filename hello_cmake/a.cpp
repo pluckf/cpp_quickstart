d503b44b063b1e0effb13df4b33e1af4645d7a03
JiangYuanShang <139546039+pluckf@users.noreply.github.com>
2026-08-12 14:50:01 +0800

    创建简单cpp项目，并用cmake编译好

---
+++ hello_cmake/a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+    int a = 5;
+	const char* p = "Hello, World!";
+	std::cout << "Hello, C++!" << std::endl;
+	return 10;
+}
---
+++ hello_cmake/out/build/GCC 8.1.0 x86_64-w64-mingw32/CMakeFiles/a_exec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_exec.dir/a.cpp.obj"
+  "CMakeFiles/a_exec.dir/a.cpp.obj.d"
+  "a_exec.exe"
+  "a_exec.exe.manifest"
+  "a_exec.pdb"
+  "liba_exec.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
